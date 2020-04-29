cc6a4c29963642d7065abee305fe05ab6323db25
zadca123 <46352587+zadca123@users.noreply.github.com>
2020-04-29 17:34:34 +0200

    Add files via upload

--- wd_cw06_23-04-2020/zad04.py
+++ wd_cw06_23-04-2020/zad04.py
@@ -1,14 +1,13 @@
 #Zad 4
 # nie gotowe
-import numpy as np  #nie rozumiem funkcji logspace
-n = 2
-m = 4
-c = np.logspace(np.log2(2) , 16, 4)
-print(c)
-# def foo(n,m):
-#     return [n**(i+1) for i in range(m)]
+import numpy as np  #nie do końca rozumiem funkcji logspace
+# n = 2
+# m = 4
+# c = np.logspace(np.log2(2) , 16, 4)
+# print(c)
+# print(np.log2(4))
 
-# macierz = foo(2,4)
-# print(macierz)
-
-print(np.log2(4))
+def foo(n,m):
+    return [n**(i+1) for i in range(m)]
+macierz = foo(2,4)
+print(macierz)
