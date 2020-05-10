1cb0519a2f0d87d0390932804233bce53ebd3580
zadca123 <46352587+zadca123@users.noreply.github.com>
2020-05-10 23:36:53 +0200

    Add files via upload

--- wd_cw08_30-04-2020/zad02_d.py
+++ wd_cw08_30-04-2020/zad02_d.py
@@ -1,25 +1,12 @@
 # Zad 2d
-# niegotowe
+# gotowe
 # cd wd_cw08_30-04-2020/
 import pandas as pd
 import xlrd
 import openpyxl
 
-x = pd.read_excel('imiona.xlsx',delimiter=';')    # Rok, Imie, Liczba, Plec
-
+x = pd.read_excel('imiona.xlsx')    # Rok, Imie, Liczba, Plec
 df = pd.DataFrame(x,columns=['Rok','Imie','Liczba','Plec'])
-
-# print(df.groupby(['Rok']).agg({'Liczba':['sum']}) & (df.where(['Rok' == '2005'])))
-# print(df[(df['Rok'])])
-
-# print(df.groupby(['Liczba']).agg({'Liczba':['sum']}))
-# print(df[df[]])
-# print(df[(df['Rok'] <= 2005) & (df['Imie'] == "ADAM")])
-# print(df[(df['Rok'] <= 2005).agg({'Liczba': ['sum']})])
-
-
-# print(df.groupby(['Liczba']).agg({'Liczba':['sum']}))
-a = df['Rok'] <=2005
-# b = df.groupby((['Rok']).agg({'Liczba':['sum']}))
-c = df.groupby
-print(df[a])
+mask = df[df['Rok'] <= 2005]
+mask2 = mask.agg({'Liczba':['sum']})
+print(mask2)
